299e115de455d5ae3d99712793cf8f00b8b8fdd1
huhui <huhui@dangbei.com>
2020-08-17 13:35:13 +0800

    做统一的gradle配置

--- app/src/main/java/com/btg/mvvm/MainActivity.java
+++ app/src/main/java/com/btg/mvvm/MainActivity.java
@@ -4,6 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+/**
+ * @创建者 567
+ * @创建时间 2020/8/14 15:00
+ * @描述
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
---
+++ lib_common/src/main/java/com/btg/common/base/BaseActivity.java
@@ -0,0 +1,10 @@
+package com.btg.common.base;
+
+/**
+ * @创建者 567
+ * @创建时间 2020/8/17 11:00
+ * @描述
+ */
+public class BaseActivity {
+
+}
